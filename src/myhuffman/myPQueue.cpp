8fb187fd7a0a14801b41e2fb79e44cc32ad8a219
xsun89 <sunxin89@gmail.com>
2014-06-25 05:56:10 -0700

    commit

--- src/myhuffman/myPQueue.cpp
+++ src/myhuffman/myPQueue.cpp
@@ -7,9 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "myPQueue.h"
-void initMyPQueue(myPQueue** queue)
-{
-	(*queue) = (myPQueue*)malloc(sizeof(myPQueue));
+void initMyPQueue(myPQueue** queue) {
+	(*queue) = (myPQueue*) malloc(sizeof(myPQueue));
 	(*queue)->size = 0;
 	(*queue)->first = NULL;
 
@@ -26,21 +25,50 @@ void addMyPQueue(myPQueue** queue, TYPE val, unsigned int priority)
 	myPQueueNode* node = (myPQueueNode*)malloc(sizeof(myPQueueNode));
 	node->priority = priority;
 	node->val = val;
-	if((*queue)->first == NULL)
+	if((*queue)->first == NULL || (*queue)->size == 0 )
 	{
+		node->nextQNode = NULL;
 		(*queue)->first = node;
 		(*queue)->size++;
 		return;
-	}else
+	} else
 	{
-
+		if(priority <= (*queue)->first->priority)
+		{
+			node->nextQNode = (*queue)->first;
+			(*queue)->first = node;
+			(*queue)->size++;
+			return;
+		} else {
+			myPQueueNode* p = (*queue)->first;
+			while(p)
+			{
+				if(priority <= p->priority)
+					break;
+				p = p->nextQNode;
+			}
+			node->nextQNode = p->nextQNode;
+			p->nextQNode = node;
+			(*queue)->size++;
+			return;
+		}
 	}
+
 }
 
 TYPE getMyPQueue(myPQueue** queue)
 {
 	TYPE htNode;
+	if((*queue)->size <= 0 || (*queue)->first == NULL)
+	{
+		printf("\nQueue is empty.\n");
+	}else
+	{
+		htNode = (*queue)->first->val;
+		(*queue)->first = (*queue)->first->nextQNode;
+		(*queue)->size--;
+	}
+
 	return htNode;
 }
-
 
